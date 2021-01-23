e6fe2f36fc7ab02dd40aa92f195ee6d89b720065
AM429 <ziyadmatt@gmail.com>
2021-01-23 03:15:53 +0200

    Sync

--- Caching/CacheWorker.py
+++ Caching/CacheWorker.py
@@ -19,14 +19,14 @@ class LRUIndex(object):
 			return -1
 	
 	def set(self, key, value):
-		k = 0
+		if not isinstance(key, tuple):
+			raise KeyError("Key is not a Tuple")
 		try:
-			k, v = self.cache.pop(key)
+			self.cache.pop(key)
 		except KeyError:
 			if len(self.cache) >= self.capacity:
 				self.cache.popitem(last=False)
-		
-		self.cache[k] = value
+		self.cache[key] = value
 
 
 class DeckPagePool(object):
@@ -35,7 +35,7 @@ class DeckPagePool(object):
 		self._page_size = page_size
 		self.serialization_path = path
 		self.cached = []
-		self.index = LRUIndex()
+		self.index = LRUIndex(4)
 		
 		self._elements = {}
 		self._min_id = datetime.fromtimestamp(float(0))
@@ -43,10 +43,12 @@ class DeckPagePool(object):
 	
 	def __setitem__(self, note_id, note):
 		if getsizeof(self._elements) > self._page_size:
-			filename = self.serialization_path.as_posix() + f'/{self.page_id}_{self._min_id}_{self._max_id}'
+			filename = self.serialization_path.as_posix() + f'/{self.page_id}_{str(datetime.timestamp(self._min_id))}' \
+			                                                f'_{str(datetime.timestamp(self._max_id))}'
 			with open(filename, 'w') as f:
 				pickle.dump(self._elements, f)
 			self.cached.append((float(datetime.timestamp(self._min_id)), float(datetime.timestamp(self._max_id))))
+			self._reset()
 		else:
 			self._elements[note_id] = note
 			if datetime.fromtimestamp(float(note_id)) < self._min_id:
@@ -54,9 +56,15 @@ class DeckPagePool(object):
 			if datetime.fromtimestamp(float(note_id)) > self._max_id:
 				self._max_id = datetime.fromtimestamp(float(note_id))
 	
-	def __getitem__(self, item):
-		pass
-	
+	def __getitem__(self, note_id):
+		if note_id in self._elements.keys():
+			return self._elements[note_id]
+		elif res := self.index.get(float(note_id)) != -1:
+			return res
+		else:
+			# unpickle the file and load it into the index
+			pass
+
 	def _reset(self):
 		self._min_id = datetime.fromtimestamp(float(0))
 		self._max_id = datetime.fromtimestamp(float(0))
